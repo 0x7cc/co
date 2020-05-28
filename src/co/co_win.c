56fa0a9412bd1fde1d0d320d3e5cd2a18de86a67
liaowuping <348984539@qq.com>
2020-05-28 14:06:09 +0800

    warnings on windows

--- src/co/co_win.c
+++ src/co/co_win.c
@@ -1,17 +1,17 @@
 
-#if WIN32
+#if _WIN32
 
 #include "co/co.h"
 #include <windows.h>
 
 co_int co_thread_create (co_func func, void* data)
 {
-  return CreateThread (NULL, NULL, func, data, NULL, NULL);
+  return (co_int)CreateThread (NULL, NULL, (LPTHREAD_START_ROUTINE)func, data, NULL, NULL);
 }
 
 co_int co_thread_join (co_int tid)
 {
-  return WaitForSingleObject (tid, INFINITE);
+  return WaitForSingleObject ((HANDLE)tid, INFINITE);
 }
 
 #endif // WIN32
