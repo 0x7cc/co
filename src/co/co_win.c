4dc63848fc696047611cb3bb1b4cdd56e00c6092
r720.ubuntu <348984539@qq.com>
2020-05-31 02:42:52 +0800

    windows thread

--- src/co/co_win.c
+++ src/co/co_win.c
@@ -4,9 +4,26 @@
 #include "co/co.h"
 #include <windows.h>
 
+typedef struct
+{
+  co_func func;
+  void*   data;
+} threadCtx;
+
+static DWORD WINAPI thread_start_routine (LPVOID lpThreadParameter)
+{
+  threadCtx* ctx = (threadCtx*)lpThreadParameter;
+  ctx->func (ctx->data);
+  co_free (ctx);
+  return 0;
+}
+
 co_int co_thread_create (co_func func, void* data)
 {
-  return (co_int)CreateThread (NULL, NULL, (LPTHREAD_START_ROUTINE)func, data, NULL, NULL);
+  threadCtx* ctx = (threadCtx*)co_alloc (sizeof (threadCtx));
+  ctx->func      = func;
+  ctx->data      = data;
+  return (co_int)CreateThread (NULL, NULL, thread_start_routine, ctx, NULL, NULL);
 }
 
 co_int co_thread_join (co_int tid)
