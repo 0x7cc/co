9a1c3c4590639131c863542128f963be7a65ebe0
0x7cc <228791391@qq.com>
2020-06-14 23:53:03 +0800

    异步send和write接口问题

--- src/co/co_macos.c
+++ src/co/co_macos.c
@@ -20,7 +20,6 @@ typedef struct
   void*   data;
 } threadCtx;
 
-typedef int (*sys_puts_t) (const char* s);
 typedef ssize_t (*sys_recv_t) (int sockfd, void* buf, size_t len, int flags);
 typedef ssize_t (*sys_send_t) (int sockfd, const void* buf, size_t len, int flags);
 typedef ssize_t (*sys_read_t) (int fd, void* buf, size_t count);
@@ -28,7 +27,6 @@ typedef ssize_t (*sys_write_t) (int fd, const void* buf, size_t count);
 
 struct
 {
-  sys_puts_t  sys_puts;
   sys_recv_t  sys_recv;
   sys_send_t  sys_send;
   sys_read_t  sys_read;
@@ -112,15 +110,19 @@ ssize_t send (int sockfd, const void* buf, size_t len, int flags) {
     int flags = fcntl (sockfd, F_GETFL, 0);
     fcntl (sockfd, F_SETFL, flags | O_NONBLOCK);
   }
-  register int ret = 0;
-  while (1) {
+  register int ret       = 0;
+  register int sentbytes = 0;
+  while (sentbytes != len) {
     co_yield_ ();
     ret = hooks.sys_send (sockfd, buf, len, flags);
-    if (ret == -1 && errno == EAGAIN)
-      continue;
-    return ret;
+    if (ret == -1) {
+      if (errno == EAGAIN)
+        continue;
+      return ret;
+    }
+    sentbytes += ret;
   }
-  return -1; // ???
+  return sentbytes;
 }
 
 ssize_t read (int fd, void* buf, size_t count) {
@@ -145,27 +147,25 @@ ssize_t write (int fd, const void* buf, size_t count) {
     int flags = fcntl (fd, F_GETFL, 0);
     fcntl (fd, F_SETFL, flags | O_NONBLOCK);
   }
-  register int ret = 0;
-  while (1) {
+  register int ret       = 0;
+  register int sentbytes = 0;
+  while (sentbytes != count) {
     co_yield_ ();
     ret = hooks.sys_write (fd, buf, count);
-    if (ret == -1 && errno == EAGAIN)
-      continue;
-    return ret;
+    if (ret == -1) {
+      if (errno == EAGAIN)
+        continue;
+      return ret;
+    }
+    sentbytes += ret;
   }
-  return -1; // ???
-}
-
-int puts (const char* s) {
-  co_yield_ ();
-  return hooks.sys_puts (s);
+  return sentbytes;
 }
 
 #endif
 
 void co_init_hooks () {
 #if CO_ENABLE_HOOKS
-  hooks.sys_puts  = (sys_puts_t)dlsym (RTLD_NEXT, "puts");
   hooks.sys_recv  = (sys_recv_t)dlsym (RTLD_NEXT, "recv");
   hooks.sys_send  = (sys_send_t)dlsym (RTLD_NEXT, "send");
   hooks.sys_read  = (sys_read_t)dlsym (RTLD_NEXT, "read");
